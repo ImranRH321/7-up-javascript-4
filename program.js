11fe1056e3c58e006a27952aff3db4700a5d7bf4
ImranRH321 <imranrh247@gmail.com>
2023-03-08 22:41:33 +0600

    problem1-done

--- program.js
+++ program.js
@@ -1,1 +1,16 @@
-console.log('program hi ');
+
+// first problem 
+// +++++++++++++++++++++++++++
+ function mindGame(number) {
+    if(typeof number === "number"&& number >= 0){
+      let total = number * 3;
+      total  +=  10; 
+      total /= 2
+      total -= 5;
+      return total;
+    }else{
+        return 'please provide positive number';
+    }
+ }
+const gameNumber =  mindGame(50)
+
