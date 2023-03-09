255ab35e64beb8110cf55c6895cce02ee8160014
ImranRH321 <imranrh247@gmail.com>
2023-03-09 09:26:32 +0600

    done-as

--- program.js
+++ program.js
@@ -1,5 +1,5 @@
-// first problem - 1
-// +++++++++++++++++++++++++++
+// first problem - 1.
+// Description:  This is function name use mindGame and receive perimeter and check number then total calculations and  outport for user
 function mindGame(number) {
   if (typeof number === "number" && number >= 0) {
     let total = number * 3;
@@ -11,10 +11,10 @@ function mindGame(number) {
     return "please provide positive number";
   }
 }
-const gameNumber = mindGame(50);
+const gameNumber = mindGame(33);
 
-// Second problem - 2
-// +++++++++++++++++++++++++++
+// Second problem - 2.
+// description:  this function work input a String character then check length ar result give this even or add
 function evenOdd(string) {
   if (typeof string === "string" && string.length >= 0) {
     if (string.trim().length % 2 === 0) {
@@ -26,10 +26,10 @@ function evenOdd(string) {
     return "Please provide String character  ?";
   }
 }
-const names = evenOdd("-2");
+const names = evenOdd("chatgpt");
 
 // Third problem -3
-// +++++++++++++++++++++++++++
+// description: this is function work same one input then check 7 grater then 0 or Qual then input Subtraction - 7 other then multiply input parameter return
 function isLGSeven(number) {
   if (typeof number === "number") {
     if (number <= 7) {
@@ -41,11 +41,10 @@ function isLGSeven(number) {
     return "please provide  number";
   }
 }
-
 const isNumber = isLGSeven(6);
 
 // Four problem - 4
-// +++++++++++++++++++++++++++
+// description: this function work same array check positive number and negative number finlay all bad data total return ;
 function findingBadData(array) {
   if (Array.isArray(array)) {
     let badData = [];
@@ -59,28 +58,31 @@ function findingBadData(array) {
     return "please provide Array";
   }
 }
-const numberArray = [10, 12, "-4", "-3", -9, -5, -33, -55];
+
+const numberArray = [-4, -9, -5, -33, -55];
 const totalBadData = findingBadData(numberArray);
 
 // Five problem - 5
-// +++++++++++++++++++++++++++
+//  description: this is function 3types parameter and check number  then first, second, third total 3 friends output total diamond.  Grater then 2000  Subtraction Other wish total result Subtraction 2000;
 function gemsToDiamond(number1, number2, number3) {
-  if(typeof number1 === "number" && typeof number2 === "number" && typeof number3 === "number"){
+  if (
+    typeof number1 === "number" &&
+    typeof number2 === "number" &&
+    typeof number3 === "number"
+  ) {
     const firstFriendGems = number1 * 21;
     const secondFriendGems = number2 * 32;
     const thirdFriendGems = number3 * 43;
     let total = firstFriendGems + secondFriendGems + thirdFriendGems;
-    //   console.log(total);
     if (total > 2000) {
       total -= 2000;
       return total;
     } else {
       return total;
     }
-  }else{
+  } else {
     return "please provide valid number";
   }
-   
 }
 
-const gemsPoint = gemsToDiamond(20, 200, 50);
+const gemsPoint = gemsToDiamond(100, 5, 1);
