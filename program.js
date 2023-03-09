45c8b86d5e38d5d058023887a11fc377485dd519
ImranRH321 <imranrh247@gmail.com>
2023-03-09 08:52:26 +0600

    done

--- program.js
+++ program.js
@@ -1,62 +1,86 @@
-
 // first problem - 1
 // +++++++++++++++++++++++++++
- function mindGame(number) {
-    if(typeof number === "number"&& number >= 0){
-      let total = number * 3;
-      total  +=  10; 
-      total /= 2
-      total -= 5;
-      return total;
-    }else{
-        return 'please provide positive number';
-    }
- }
-const gameNumber =  mindGame(50)
-
+function mindGame(number) {
+  if (typeof number === "number" && number >= 0) {
+    let total = number * 3;
+    total += 10;
+    total /= 2;
+    total -= 5;
+    return total;
+  } else {
+    return "please provide positive number";
+  }
+}
+const gameNumber = mindGame(50);
 
 // Second problem - 2
 // +++++++++++++++++++++++++++
-function  evenOdd(string) {
-    if(typeof string === "string" && string.length >= 0){
-        if(string.trim().length % 2  === 0){
-          return "even number";
-        }else{
-            return "odd number";
-        }
-    }else{
-        return "Please provide String character  ?"
+function evenOdd(string) {
+  if (typeof string === "string" && string.length >= 0) {
+    if (string.trim().length % 2 === 0) {
+      return "even number";
+    } else {
+      return "odd number";
     }
+  } else {
+    return "Please provide String character  ?";
+  }
 }
-const names =  evenOdd("-2");
-
+const names = evenOdd("-2");
 
 // Third problem -3
 // +++++++++++++++++++++++++++
-function isLGSeven (number) {
-    
-    if(number <= 7){
-      return number - 7
-    }else{
-        return number * number;
+function isLGSeven(number) {
+  if (typeof number === "number") {
+    if (number <= 7) {
+      return number - 7;
+    } else {
+      return number * number;
     }
+  } else {
+    return "please provide  number";
+  }
 }
 
-const isNumber =  isLGSeven(6)
+const isNumber = isLGSeven(6);
 
 // Four problem - 4
 // +++++++++++++++++++++++++++
 function findingBadData(array) {
+  if (Array.isArray(array)) {
     let badData = [];
     for (const number of array) {
-        if(number < 0){
-            badData.push(number)
-        }
+      if (number < 0) {
+        badData.push(number);
+      }
     }
     return badData.length;
+  } else {
+    return "please provide Array";
+  }
+}
+const numberArray = [10, 12, "-4", "-3", -9, -5, -33, -55];
+const totalBadData = findingBadData(numberArray);
+
+// Five problem - 5
+// +++++++++++++++++++++++++++
+function gemsToDiamond(number1, number2, number3) {
+  if(typeof number1 === "number" && typeof number2 === "number" && typeof number3 === "number"){
+    const firstFriendGems = number1 * 21;
+    const secondFriendGems = number2 * 32;
+    const thirdFriendGems = number3 * 43;
+    let total = firstFriendGems + secondFriendGems + thirdFriendGems;
+    //   console.log(total);
+    if (total > 2000) {
+      total -= 2000;
+      return total;
+    } else {
+      return total;
+    }
+  }else{
+    return "please provide valid number";
+  }
+   
 }
 
-
-const numberArray = [3,4,5,-77, 0,100 -22, -50,-34, -33]
-const number = findingBadData(numberArray)
-
+const gemsPoint = gemsToDiamond(20, 200, 50);
